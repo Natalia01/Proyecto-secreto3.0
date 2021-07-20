0075569844f4c81851df919c2a5aa342ae574f3f
Natalia Fernanda <nati@MacBook-Pro-de-Natalia.local>
2021-07-20 11:25:55 -0400

    Tabla creada con detalles

---
+++ pages/admin/details.js
@@ -0,0 +1,8 @@
+const details = () =>
+{
+    return(
+        <h1>Holaaaaaa</h1>
+    )
+}
+
+export default details;
--- pages/admin/issueAdmin.js
+++ pages/admin/issueAdmin.js
@@ -1,7 +1,95 @@
+import { Table, Tag, Space } from 'antd';
+import React from 'react';
+import 'antd/dist/antd.css';
+import Link from 'next/link'
+import { Alert } from 'antd';
+
 const issuesAdmin = () => {
-    return (
-        <h1>HOLI</h1>
-    )
+  const columns = [
+    {
+      title: 'N° Orden',
+      dataIndex: 'name',
+      key: 'name',
+      render: text => <a>{text}</a>,
+    },
+    {
+      title: 'Fecha',
+      dataIndex: 'age',
+      key: 'age',
+    },
+    {
+      title: 'Descripción',
+      dataIndex: 'address',
+      key: 'address',
+    },
+    {
+      title: 'Área',
+      key: 'tags',
+      dataIndex: 'tags',
+      render: tags => (
+        <>
+          {tags.map(tag => {
+            let color = tag.length > 5 ? 'geekblue' : 'green';
+            if (tag === 'loser') {
+              color = 'volcano';
+            }
+            return (
+              <Tag color={color} key={tag}>
+                {tag.toUpperCase()}
+              </Tag>
+            );
+          })}
+        </>
+      ),
+    },
+    {
+      title: 'Acciones',
+      key: 'action',
+      render: (text, record) => (
+        <Space size="middle">
+          <Link href = '/admin/details'>
+          <a>Ver detalles</a>
+          </Link>
+          
+          <a>Revisado</a>
+        </Space>
+      ),
+    },
+  ];
+  
+  const data = [
+    {
+      key: '1',
+      name: 'John Brown',
+      age: 32,
+      address: 'New York No. 1 Lake Park',
+      tags: ['nice', 'developer'],
+    },
+    {
+      key: '2',
+      name: 'Jim Green',
+      age: 42,
+      address: 'London No. 1 Lake Park',
+      tags: ['loser'],
+    },
+    {
+      key: '3',
+      name: 'Joe Black',
+      age: 32,
+      address: 'Sidney No. 1 Lake Park',
+      tags: ['cool', 'teacher'],
+    },
+  ];
+
+  return(
+    <div>
+      <h1>Administrador de problemas</h1>
+      <Table columns={columns} dataSource={data}/>
+    </div> 
+  
+  )
+
 }
+
 
 export default issuesAdmin
